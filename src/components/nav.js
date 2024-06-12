d1f21e8995793ae57367d34d43b1fdcb09681d0f
Daniel Balloch <danielpbballoch@gmail.com>
2024-06-12 17:15:02 +1200

    add menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,8 +6,8 @@ import Hamburger from "../components/hamburger"
 
 const Wrapper = styled.div`
 width: 100%;
-position: fixed;
-padding-top: 5px;
+position: absolute;
+padding-top: 15px;
 // overflow-x: hidden;
 z-index: 200;
 .active {
@@ -71,6 +71,9 @@ z-index: 200;
 }
 }
 @media(max-width: 1420px){
+    .nav-bobcat-products {
+    margin-top: 80px!important;
+    }
     .navbar {
         position: fixed;
         justify-content: flex-end;
@@ -135,12 +138,257 @@ z-index: 200;
     display: none;
 }
 }
+
+.close-btn {
+position: absolute;
+// right: 5vw;
+right: 0;
+box-sizing: border-box;
+padding: 15px 20px;
+height: 50px;
+width: calc(0.333 * 90vw);
+// max-width: 500px;
+text-align: right;
+border-radius: 0 10px 0 0;
+:hover {
+    background-color: rgba(0,0,0,0.04);
+    cursor: pointer;
+    // color: #ff3600;
+}
+}
+.nav-bobcat-products {
+position: absolute;
+left: 5%;
+z-index: 200!important;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+margin: auto;
+width: 90vw;
+height: auto;
+background-color: rgba(255,255,255,0.98);
+border-radius: 10px;
+transition: .3s;
+opacity: 1;
+.products-link {
+    text-decoration: none;
+    color: black;
+    // max-width: 500px;
+    width: 33.3%;
+    text-align: center;
+    :hover {
+     background-color: rgba(0,0,0,0.04)
+    }
+   
+}
+}
+.nav-row {
+    display: flex;
+    :first-of-type {
+    margin-top: 50px;}
+}
+.nav-item {
+    align-items: center;
+    box-sizing: border-box;
+    // max-width: 500px;
+    width: 33.3%;
+    display: flex;
+    padding: 10px 20px;
+    transition: .3s;
+    :hover {
+     // background-color: #B00E23;
+    // background-color: #f1f1f1;
+    background-color: rgba(0,0,0,0.04);
+    // mix-blend-mode: multiply;
+    // background-color: #ff3600;
+    // color:white;
+    cursor: pointer;
+    }
+}
+.item-image {
+    img {
+    object-fit: contain!important;
+    }
+    div {
+    height: 100%;
+    }
+}
+.item-content {
+    // width: 50%;
+    height: fit-content;
+    h2 {
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-size: 20px;
+    }
+    p {
+    // width: 250px;
+    
+    font-size: 14px;
+    margin: 10px 0;
+    }
+    a {
+    font-weight: 600;
+    }
+    padding: 10px;
+}
+.nav-fade {
+    opacity: 0!important;
+    display: none;
+}
+.nav-none {
+    display: none;
+}
+.bg-dim {
+z-index: 100;
+position: fixed;
+top: 0;
+width: 100vw;
+height: 100vh;
+background-color: rgba(0,0,0,0.7);
+animation: appear 1s;
+}
+.bg-dim-off {
+display: none;
+opacity: 0;
+}
+@keyframes appear {from {opacity: 0}to {opacity: 1;}}
+
+.nav-fade {
+
+}
+//services section css
+.nav-services {
+position: absolute;
+top: 95px;
+left: 40%;
+z-index: 200!important;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+margin: auto;
+width: auto;
+height: auto;
+background-color: rgba(255,255,255,0.98);
+border-radius: 10px;
+transition: .3s;
+opacity: 1;
+.nav-item {
+    padding: 2px;
+    position: relative;
+    
+    h2 {
+        margin-top: 0;
+        color: white;
+        font-size: 16px;
+        width: 90%;
+        text-align: center;
+        z-index: 200;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    :hover {
+        .item-image {
+            filter: brightness(0.6);
+        }
+    }
+}
+.products-link {
+    text-decoration: none;
+    color: black;
+    // max-width: 500px;
+    width: 364px;
+    text-align: center;
+    :hover {
+     background-color: rgba(0,0,0,0.04);
+    }
+}
+.close-btn {
+width: 368px!important;
+}
+.nav-row {
+    padding: 0 2px;
+    // margin: 0;
+}
+.item-image {
+    transition: .3s;
+    height: 180px;
+    filter: brightness(0.3);
+}
+}
+//company section css
+.nav-company {
+    position: absolute;
+    top: 95px;
+    left: 40%;
+    z-index: 200!important;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+    width: auto;
+    height: auto;
+    background-color: rgba(255,255,255,0.98);
+    border-radius: 10px;
+    transition: .3s;
+    opacity: 1;
+    .close-btn {
+        width: 190px;
+        color: white;
+        z-index: 200;
+    }
+    .nav-wrapper {
+        display: flex;
+        flex-direction: row;
+    }
+    .item-image {
+        border-radius: 0 10px 10px 0;
+        filter: brightness(0.8);
+        height: 372px;
+        margin: 4px 4px 4px 0;
+    }
+    .nav-column {
+        display: flex;
+        flex-direction: column;
+        height: 380px;
+        width: 400px;
+    }
+    .company-item {
+        width: 400px;
+        flex-direction: column;
+        width: 100%;
+        padding: 10px 20px;
+        box-sizing: border-box;
+        transition: .3s;
+        :hover {
+            background-color: rgba(0,0,0,0.04);
+            cursor: pointer;
+        }
+        :last-of-type {
+            margin-top: auto;
+        }
+        h2 {
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+        p{
+            font-size: 14px;
+            margin: 0;
+        }
+    }
+}
 `
 
 
 
 export default function Nav(){
     const [active, setActive] = useState(false)
+    const [menu, setMenu] = useState()
     return(
         <Wrapper id="top">
             <div className="navbar">
@@ -149,14 +397,161 @@ export default function Nav(){
                     {/* ORDINARY DIGITAL &copy; */}
                 </Link>
                 <div className="right-content">
-                    <Link to="/#products">Bobcat<span>®</span> Products</Link>
-                    <Link to="/#services">Our Services</Link>
-                    <Link to="/#company">Company</Link>
-                    <Link to="/#careers">Careers</Link>
-                    <a className={active? "active call" : "call"} href="tel:07-957-2695">+07 957 2695</a>
+                    <Link to="/#products" onMouseEnter={() => setMenu("products")}>Bobcat<span>®</span> Products</Link>
+                    <Link to="/#services" onMouseEnter={() => setMenu("services")}>Our Services</Link>
+                    <Link to="/#company"  onMouseEnter={() => setMenu("company")}>Company</Link>
+                    <Link to="/#careers"  onMouseEnter={() => setMenu("careers")}>Careers</Link>
+                    <a className={active? "active call" : "call"} onMouseEnter={() => setMenu("")} href="tel:07-957-2695">+07 957 2695</a>
                 </div>
                 <Hamburger setActive={() => setActive(!active)} active={active}/>
             </div>
+            <div className={menu === "products" || menu === "services" || menu === "company" || menu === "careers"? "bg-dim" : "bg-dim bg-dim-off"} onClick={() => setMenu("")}/>
+            <div className={menu == "products"? "nav-bobcat-products":"nav-fade"}>
+                <div className="close-btn" onClick={() => setMenu("")}>Close</div>
+                <div className="nav-wrapper">
+                    <div className="nav-row">
+                        <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/Loader2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Loaders</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Loaders</a>
+                            </div>
+                        </div>
+                        <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/MiniExcavator2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Mini Excavators</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Mini Excavators</a>
+                            </div>
+                        </div>
+                        <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/Tractor2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Tractors</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Tractors</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="nav-row">
+                    <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/Forklift2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Forklifts</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Forklifts</a>
+                            </div>
+                        </div>
+                        <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/Utility2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Utility/Other</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Utility/Other</a>
+                            </div>
+                        </div>
+                        <div className="nav-item">
+                            <StaticImage className="item-image" src="../images/nav-images/products/Attachment2.png" alt="thoughtfulHQ" />
+                            <div className="item-content">
+                                <h2>Attachments</h2>
+                                <p>Skid-Steer, Compact Track, Compact Wheel, Backhoe - we have a large range of Bobcat loaders to suit the needs of your job.</p>
+                                <a>View Attachments</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="#AllProducts" className="products-link"><h2>View All Products</h2></a>
+            </div>
+
+            <div className={menu == "services"? "nav-services":"nav-fade"}>
+                <div className="close-btn" onClick={() => setMenu("")}>Close</div>
+                <div className="nav-wrapper">
+                    <div className="nav-row">
+                        <div className="nav-item">
+                            <h2>Heavy Diesel Truck Repair</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/Hevy.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>Earthmoving Equipment Repairs</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/Earthmoving.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>Cars, Vans & Ute Repairs</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/Cars.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>Forklift Repairs</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/Forklift.png" alt="thoughtfulHQ" />
+                        </div>
+                    </div>
+                    <div className="nav-row">
+                        <div className="nav-item">
+                            <h2>Auto Electrical</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/AutoElectrical.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>Tyres, Suspension & Hydraulics</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/TyresSuspension.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>WOF & COF</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/WOF.png" alt="thoughtfulHQ" />
+                        </div>
+                        <div className="nav-item">
+                            <h2>Chainsaws, Mowers & Small Machines</h2>
+                            <StaticImage className="item-image" src="../images/nav-images/services/Small.png" alt="thoughtfulHQ" />
+                        </div>
+                    </div>
+                </div>
+                <a href="#AllProducts" className="products-link"><h2>View All Services</h2></a>
+            </div>
+
+            <div className={menu == "company"? "nav-company":"nav-fade"}>
+                <div className="close-btn" onClick={() => setMenu("")}>Close</div>
+                <div className="nav-wrapper">
+                    <div className="nav-column">
+                        <div className="company-item">
+                            <h2>About Us</h2>
+                            <p>This is a short description under the service which opens on hover and hooks the user to read more.</p>
+                        </div>
+                        <div className="company-item">
+                            <h2>Our Team & Partners</h2>
+                            <p>This is a short description under the service which opens on hover and hooks the user to read more.</p>
+                        </div>
+                        <div className="company-item">
+                            <h2>Support Center</h2>
+                            <p>This is a short description under the service which opens on hover and hooks the user to read more.</p>
+                        </div>
+                        <div className="company-item">
+                            <h2>View Company</h2>
+                        </div>
+                    </div>
+                    <StaticImage className="item-image" src="../images/nav-images/services/Hevy.png" alt="thoughtfulHQ" />
+                </div>
+            </div>
+
+            <div className={menu == "careers"? "nav-company":"nav-fade"}>
+                <div className="close-btn" onClick={() => setMenu("")}>Close</div>
+                <div className="nav-wrapper">
+                    <div className="nav-column">
+                        <div className="company-item">
+                            <h2>Working For Us</h2>
+                            <p>This is a short description under the service which opens on hover and hooks the user to read more.</p>
+                        </div>
+                        <div className="company-item">
+                            <h2>Positions Available</h2>
+                            <p>This is a short description under the service which opens on hover and hooks the user to read more.</p>
+                        </div>
+                        <div className="company-item">
+                            <h2>View Careers</h2>
+                        </div>
+                    </div>
+                    <StaticImage className="item-image" src="../images/nav-images/services/AutoElectrical.png" alt="thoughtfulHQ" />
+                </div>
+            </div>
+            
             <div className={active? "sidedrawer" : "sidedrawer drawertoggle"}>
                 <Link to="/#top" onClick={() => setActive(!active)}>Home</Link>
                 <Link to="/projects">Bobcat<span>®</span> Products</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import styled from "@emotion/styled"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Title = styled.div`
+// background-color: black;
+//   color: white;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  margin: auto;
+  h1 {
+    color: #222;
+    margin: auto;
+    width: 1350px;
+    font-size: 40px;
+    margin-bottom: 20px;
+    // margin-bottom: 0;
+  }
+  p {
+    margin: auto;
+    width: 1350px;
+    font-size: 20px;
+   
+  }
+`
+
+const Services = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+.service-item {
+  width: 440px;
+  margin: 10px;
+}
+`
 
 
 const IndexPage = () => {
@@ -8,9 +42,53 @@ const IndexPage = () => {
     <Layout>
     <main>
       <Hero/>
-      <div>
-      {/* <h1 style={{padding:80}}>YOUR TRUSTED  WAIKATO MECHANIC AND BOBCAT DEALER.<br/><br/> WE DELIVER A FULL SUITE OF MECHANICAL & ENGINEERING SERVICES, FROM HAND TOOL REPAIR TO FULL DIESEL FLEETS & MACHINARY.</h1> */}
-      </div>
+      <Title>
+        {/* <h1>FROM HAND TOOL REPAIR,<br/>FULL DIESEL FLEETS,<br/>MACHINARY ENGINEERING,<br/>WE DELIVER.</h1> */}
+        <h1>WE SERVICE, REPAIR AND MAINTAIN FULL BUSINESS FLEETS, FROM DIESEL TRUCKS AND LARGE MACHINARY TO HANDTOOLS AND EQUIPMENT.</h1>
+        <p>Pro Mechanical Ltd have been providing quality service to brands like Enviro NZ, SCANIA, INTERNATIONAL, FOTON, since way back in 2012.</p>
+        <br/>
+        <p>Our focus has always been top-quality workmanship, quickly and at a great rate.</p>
+        <br/>
+        <p>With this mindset we've grown from one man and his van to a skilled team of over 40+ and now have the facility, people and skills to keep any businesses fleet/equipment serviced, safe, and ready when you need it.</p>
+      </Title>
+      <Services>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/Hevy.png" alt="thoughtfulHQ" />
+            {/* <h2>Heavy Diesel Truck Repair</h2> */}
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/Earthmoving.png" alt="thoughtfulHQ" />
+            {/* <h2>Earthmoving Equipment Repairs</h2> */}
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/Cars.png" alt="thoughtfulHQ" />
+            {/* <h2>Cars, Vans & Ute Repairs</h2> */}
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/Forklift.png" alt="thoughtfulHQ" />
+            <h2>Forklift Repairs</h2>
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/AutoElectrical.png" alt="thoughtfulHQ" />
+            <h2>Auto Electrical</h2>
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/TyresSuspension.png" alt="thoughtfulHQ" />
+            <h2>Tyres, Suspension & Hydraulics</h2>
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/WOF.png" alt="thoughtfulHQ" />
+            <h2>WOF & COF</h2>
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/nav-images/services/Small.png" alt="thoughtfulHQ" />
+            <h2>Chainsaws, Mowers & Small Machines</h2>
+        </div>
+        <div className="service-item">
+            <StaticImage className="item-image" src="../images/BobcatImage.png" alt="thoughtfulHQ" />
+            <h2>Bobcat Sales</h2>
+        </div>
+      </Services>
     </main>
     </Layout>
   )
